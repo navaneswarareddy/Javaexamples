7a2745384288790792444978683b3bfc0e5d3d83
priya <sup6iya@gmail.com>
2020-05-01 12:28:34 +0530

    Modified Addition

---
+++ src/com/naveen/Addition.java
@@ -0,0 +1,20 @@
+package com.naveen;
+
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1, num2, result;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter First number:");
+		num1 = scan.nextInt();
+		System.out.print("Enter Second number:");
+		num2 = scan.nextInt();
+		result = num1 + num2;
+		System.out.println("Adition of " + num1 + " " + "and" + " " + num2 + " " + "is: " + result);
+		scan.close();
+	}
+
+}
